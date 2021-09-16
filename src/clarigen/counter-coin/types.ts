3a355b81fd14ae132295a67738e0f05ed8759a51
aviculturist <88842484+aviculturist@users.noreply.github.com>
2021-09-16 09:17:35 -0400

    Updates to latest clarigen versions

--- src/clarigen/counter-coin/types.ts
+++ src/clarigen/counter-coin/types.ts
@@ -3,11 +3,11 @@ import { ClarityTypes, Transaction } from '@clarigen/core';
 // prettier-ignore
 export interface CounterCoinContract {
   getTokenUri: () => Transaction<string | null, null>;
-  mint: (recipient: string, amount: number) => Transaction<boolean, number>;
-  transfer: (amount: number, sender: string, recipient: string, memo: Buffer | null) => Transaction<boolean, number>;
-  getBalance: (owner: string) => Promise<ClarityTypes.Response<number, null>>;
-  getDecimals: () => Promise<ClarityTypes.Response<number, null>>;
+  mint: (recipient: string, amount: number | bigint) => Transaction<boolean, bigint>;
+  transfer: (amount: number | bigint, sender: string, recipient: string, memo: Buffer | null) => Transaction<boolean, bigint>;
+  getBalance: (owner: string) => Promise<ClarityTypes.Response<bigint, null>>;
+  getDecimals: () => Promise<ClarityTypes.Response<bigint, null>>;
   getName: () => Promise<ClarityTypes.Response<string, null>>;
   getSymbol: () => Promise<ClarityTypes.Response<string, null>>;
-  getTotalSupply: () => Promise<ClarityTypes.Response<number, null>>;
+  getTotalSupply: () => Promise<ClarityTypes.Response<bigint, null>>;
 }
